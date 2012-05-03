a3942aaa873c11599f718ba71c4d63fa96dfaf92
oceanor <admin@oceanor.net>
2012-05-03 02:04:16 +0200

    First public version: 2.0

---
+++ src/main/java/me/oceanor/OceManaBar/OceListener.java
@@ -0,0 +1,54 @@
+package me.oceanor.OceManaBar;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import com.nisovin.magicspells.events.ManaChangeEvent;
+
+public class OceListener implements Listener 
+{
+	public OceManaBar plugin;
+	
+	public OceListener(OceManaBar instance)
+	{
+		this.plugin=instance;
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onManaChange(ManaChangeEvent event)
+	{
+		if(OceManaBar.enabled && event.getPlayer().hasPermission("ocemanabar.show"))
+		{
+			if(OceManaBar.useTexture)
+			{
+				OceManaBar.gradientbars.get(event.getPlayer()).setWidth(event.getNewAmount() * (OceManaBar.width -3) / event.getMaxMana()).setDirty(true);
+			}
+			if(OceManaBar.useAscii)
+			{
+				int segments = (int) (Math.floor(event.getNewAmount() * OceManaBar.size) / event.getMaxMana());
+				String textbar = "";
+				textbar += ChatColor.DARK_GRAY + "[" + ChatColor.BLUE;
+				
+				int i;
+				for (i=0 ; i<segments; i++)
+					textbar += OceManaBar.segmentChar;
+				
+				textbar += ChatColor.BLACK;
+				
+				for (; i< OceManaBar.size; i++)
+					textbar += OceManaBar.segmentChar;
+				
+				textbar += ChatColor.DARK_GRAY + "]";
+		    
+				OceManaBar.asciibars.get(event.getPlayer()).setText(textbar).setDirty(true);
+			}
+			if(OceManaBar.showNumeric)
+			{
+				String numvalues = ChatColor.WHITE + "[" + event.getNewAmount() + "/" + event.getMaxMana() + "]";
+				OceManaBar.numericmanas.get(event.getPlayer()).setText(numvalues).setDirty(true);
+			}
+		}
+	}
+}
---
+++ src/main/java/me/oceanor/OceManaBar/OceManaBarSpoutListener.java
@@ -0,0 +1,69 @@
+package me.oceanor.OceManaBar;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.getspout.spoutapi.event.spout.SpoutCraftEnableEvent;
+import org.getspout.spoutapi.gui.Color;
+import org.getspout.spoutapi.gui.GenericGradient;
+import org.getspout.spoutapi.gui.GenericLabel;
+import org.getspout.spoutapi.gui.RenderPriority;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+public class OceManaBarSpoutListener implements Listener
+{
+	public OceManaBar plugin;
+	
+	public OceManaBarSpoutListener(OceManaBar plugin) 
+	{
+		this.plugin = plugin;
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onSpoutCraftEnable(SpoutCraftEnableEvent event)
+	{
+		SpoutPlayer player = event.getPlayer();
+		
+		if(OceManaBar.enabled && player.hasPermission("ocemanabar.show"))
+		{
+			GenericLabel numericMana = new GenericLabel();
+			GenericLabel asciiBar = new GenericLabel();
+			GenericGradient gradientBar = new GenericGradient();
+			GenericGradient gradientBackground = new GenericGradient();
+		
+			if(OceManaBar.useTexture)
+			{
+    			Color lightblue = new Color(0,191,255);
+    			Color darkblue = new Color(16,78,139);
+    			Color black = new Color(0,0,0);
+
+    			// gradient bar
+    			gradientBar.setX(OceManaBar.posX +1).setY(OceManaBar.posY +2).setWidth(OceManaBar.width -3).setHeight(OceManaBar.height - 7);
+    			gradientBar.setBottomColor(lightblue).setTopColor(darkblue).setPriority(RenderPriority.Lowest);
+
+				player.getMainScreen().attachWidget(plugin, gradientBar);
+				OceManaBar.gradientbars.put(player,gradientBar);
+				
+    			// black background / frame
+    			gradientBackground.setX(OceManaBar.posX).setY(OceManaBar.posY).setWidth(OceManaBar.width).setHeight(OceManaBar.height -3);
+    			gradientBackground.setBottomColor(black).setTopColor(black).setPriority(RenderPriority.Highest);
+
+				player.getMainScreen().attachWidget(plugin, gradientBackground);
+			}
+			
+			if(OceManaBar.useAscii)
+			{
+				asciiBar.setAuto(false).setX(OceManaBar.posX).setY(OceManaBar.posY).setWidth(OceManaBar.width).setHeight(OceManaBar.height);
+				player.getMainScreen().attachWidget(plugin, asciiBar);
+				OceManaBar.asciibars.put(player,asciiBar);
+			}
+			
+			if(OceManaBar.showNumeric)
+			{
+				numericMana.setAuto(false).setX(OceManaBar.posX + OceManaBar.width).setY(OceManaBar.posY).setWidth(35).setHeight(OceManaBar.height);
+				numericMana.setText("[100"+ "/" + "100]");
+				player.getMainScreen().attachWidget(plugin, numericMana);
+				OceManaBar.numericmanas.put(player,numericMana);
+			}
+		}
+	}
+}
